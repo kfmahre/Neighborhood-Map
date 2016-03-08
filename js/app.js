64923d3cfdc7f5f0a72cbe3d635677c306de43cc
kfmahre <kfmahre@gmail.com>
2016-03-08 03:46:11 -0500

    added a new trail to the map, and a new part to the initialTrails... something for the wikipedia links specifically so that when they are searched for something will definitely come up.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var Model = function(){
     address: "900 Kennesaw Mountain Dr, Kennesaw Georgia",
     description: "Run or hike this 2.4 mile round-trip trail to climb Kennesaw Mountain. Depending on your fitness level and how fast you attempt to get to the top, this trail can vary in difficulty. This trail is heavy volume, so expect a crowd on a nice day!",
     trailMapUrl: "https://www.google.com/maps/d/embed?mid=zZORWFp4Wxqg.kAyWKntuuMJ4&hl=en",
+    generalLoc: "Kennesaw Mountain National Battlefield Park",
     lat: "33.98363",
     lng: "-84.57836"
       },
@@ -16,6 +17,7 @@ var Model = function(){
     address: "900 Kennesaw Mountain Dr, Kennesaw Georgia",
     description: "This course is a real challenge and completing it is a huge confidence builder! This run is all uphill up a mountain road for the first half, and then back downhill for the second half. There are some very sightworthy views from the road under certian conditions.",
     trailMapUrl: "https://www.google.com/maps/d/embed?mid=zQIhibTo9Q34.k2ExBAbfkAX0&hl=en",
+    generalLoc: "Kennesaw Mountain National Battlefield Park",
     lat: "33.98515",
     lng: "-84.58278"
       },
@@ -24,6 +26,7 @@ var Model = function(){
     address: "1520 Burnt Hickory Rd NW, Marietta, GA 30064",
     description: "This trail starts and ends at two parking lots and is about three miles long. I like to park at one of the areas and then run to the other, then turn back the same way. It makes for a pretty easy and peaceful 6 mile round trip on a hot day in Georgia, as most of the way is under thick trees.",
     trailMapUrl: "https://www.google.com/maps/d/embed?mid=zQIhibTo9Q34.k1XcH0DRGg5o&hl=en",
+    generalLoc: "Noses Creek",
     lat: "33.96342",
     lng: "-84.59376"
       },
@@ -32,6 +35,7 @@ var Model = function(){
     address: "3009 Bells Ferry Rd NE Marietta, Georgia",
     description: "This trail is good for bikers and runners who are looking to train on a flat and gradually sloped pathway. If you are looking for road running away or off of roads, this trail is paved, but it is not beside a road. The path I usually take only requires crossing one road; however, it is possible to take the trail all the way to adjoining trails at Kennesaw National Battlefield Park.",
     trailMapUrl: "https://www.google.com/maps/d/embed?mid=zQIhibTo9Q34.kI1WmdKvbRTU&hl=en",
+    generalLoc: "Noonday Creek",
     lat: "34.02328",
     lng: "-84.54868"
       },
@@ -39,9 +43,19 @@ var Model = function(){
     name: "Red Top Mountain State Park",
     address: "659 Red Top Mountain Rd SE, Acworth, GA 30121",
     description: "",
+    generalLoc: "Red Top Mountain",
     trailMapUrl: "",
     lat: "34.1546",
     lng: "-84.70299"
+      },
+      {
+    name: "Silver Comet Trail",
+    address: "4573 Mavell Rd, Smyrna, GA 30008",
+    description: "No cars. Few hills. And mile after blissful mile of paved, smooth trail that just begs for a long-distance workout. The Silver Comet Trail is one of Georgia’s top trails for running, walking, cycling, or skating. It’s an epic-length, 61.5 mile trail that spans from the Atlanta suburbs in Symrna to the Alabama border. The rail trail follows the former corridor of the Silver Comet, an upscale passenger railway that ran through this scenic swath of Georgia landscape in the midcentury 1900s.",
+    generalLoc: "Silver Comet Trail",
+    trailMapUrl: "https://www.google.com/maps/d/embed?mid=z020aeEbhAZU.kf-qc7HOg6Bw",
+    lat: "33.842162",
+    lng: "-84.517838"
       }
       ]
 
@@ -156,7 +170,7 @@ var layMarkers = function() {
 
           var getWiki = function(marker) {
             infowindow.setContent("");
-            var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&limit=1&search='+trail.name+'&format=json&callback=wikiCallback';
+            var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&limit=1&search='+trail.generalLoc+'&format=json&callback=wikiCallback';
             var wikiRequestTimeout = setTimeout(function() {
                 alert("Rats! Wikipedia could not be loaded."); // Error handler
             }, 2200);
