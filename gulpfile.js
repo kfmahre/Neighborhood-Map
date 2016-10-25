5c99e0c81880dd4b9d6c0bc8fdf2395ddbb1c20f
kfmahre <kfmahre@gmail.com>
2016-10-25 12:43:13 -0400

    troubleshooting

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+
+var cssFiles = 'css/main.scss';
+
+gulp.task('sass', function(){
+  gulp.src(cssFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('Portfolio/css/*.scss', ['sass']);
+})
+
+gulp.task('init', ['sass']);
+gulp.task('default', ['sass']);
